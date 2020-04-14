5a65d50163953b3286f305ba4a5ae8db7fc06897
Adam Shahzad <adam.shahzad@outlook.com>
2020-04-14 11:50:59 +0100

    hot fix branch version to release

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "govuk-template"                   % "5.36.0-play-26",
     "uk.gov.hmrc"             %% "play-ui"                          % "7.40.0-play-26",
     "uk.gov.hmrc"             %% "bootstrap-play-26"                % "0.42.0",
-    "uk.gov.hmrc"             %% "pay-api-cor-card-payment-journey" % "[0.358.0,)",
+    "uk.gov.hmrc"             %% "pay-api-cor-card-payment-journey" % "0.360.0",
     "com.beachape"            %% "enumeratum" % "1.5.13",
     "org.typelevel"           %% "cats-core" % catsVersion,
     "org.typelevel"           %% "cats-kernel" % catsVersion,
