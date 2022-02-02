1370986762f8801a730f9521864cc2738154be10
Marcin Pucilowski <75978587+PucilowskiHMRC@users.noreply.github.com>
2022-02-02 11:57:22 +0000

    [MP][OPS-7083] populated welshtext (#39)

--- app/model/content/OriginToContentOptions.scala
+++ app/model/content/OriginToContentOptions.scala
@@ -5,11 +5,10 @@ import paysurvey.origin.SurveyOrigin.Itsa
 
 object OriginToContentOptions {
 
-  // TODO: welshtext
   def toContentOptions(origin: SurveyOrigin): Option[ContentOptions] = origin match {
     case Itsa => Option(ContentOptions(
       isWelshSupported = IsWelshSupported.yes,
-      title            = BannerTitle(englishValue = "Request a Self Assessment repayment", welshValue = Option("welshtext"))
+      title = BannerTitle(englishValue = "Request a Self Assessment repayment", welshValue = Option("Gwneud cais am ad-daliad Hunanasesiad"))
     ))
     case _ => None
   }
