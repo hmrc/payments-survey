2630d01acb307a5074d832b420d54c0451c58ca9
Andy Dwelly <andydwelly@gmail.com>
2022-11-16 15:20:44 +0000

    Migration to new hmrc mongo driver

--- app/paysurvey/journey/JourneyService.scala
+++ app/paysurvey/journey/JourneyService.scala
@@ -11,7 +11,7 @@ class JourneyService @Inject() (
     journeyRepo: JourneyRepo
 )(implicit ec: ExecutionContext) {
 
-  def insert(j: SurveyJourney): Future[Unit] = journeyRepo.insert(j).checkResult
+  def insert(j: SurveyJourney): Future[Unit] = journeyRepo.insert(j) //.checkResult
 
   def findLatestByJourneyId(journeyId: SurveyJourneyId): Future[Option[SurveyJourney]] = {
     journeyRepo.findLatestJourneyByJourneyId(journeyId)
