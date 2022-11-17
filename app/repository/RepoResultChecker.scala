6fb5e09f0a8a2d03e999f8b14de469213144ae47
Andy Dwelly <andydwelly@gmail.com>
2022-11-17 14:43:14 +0000

    Added updated findLatestByJourneyId, insert methods

--- app/repository/RepoResultChecker.scala
+++ app/repository/RepoResultChecker.scala
@@ -5,20 +5,20 @@
 
 package repository
 
-import reactivemongo.api.commands.WriteResult
-
-import scala.concurrent.{ExecutionContext, Future}
-
-object RepoResultChecker {
-  implicit class WriteResultChecker(future: Future[WriteResult]) {
-    def checkResult[T](ifOkay: => T)(implicit ec: ExecutionContext): Future[T] = future.map {
-      case writeResult: WriteResult if isNotReallyOk(writeResult) => throw new RuntimeException(writeResult.toString)
-      case _ => ifOkay
-    }
-
-    def checkResult(implicit ec: ExecutionContext): Future[Unit] = checkResult(())
-  }
-
-  private def isNotReallyOk(writeResult: WriteResult) = !writeResult.ok || writeResult.writeErrors.nonEmpty || writeResult.writeConcernError.isDefined
-
-}
+//import reactivemongo.api.commands.WriteResult
+//
+//import scala.concurrent.{ExecutionContext, Future}
+//
+//object RepoResultChecker {
+//  implicit class WriteResultChecker(future: Future[WriteResult]) {
+//    def checkResult[T](ifOkay: => T)(implicit ec: ExecutionContext): Future[T] = future.map {
+//      case writeResult: WriteResult if isNotReallyOk(writeResult) => throw new RuntimeException(writeResult.toString)
+//      case _ => ifOkay
+//    }
+//
+//    def checkResult(implicit ec: ExecutionContext): Future[Unit] = checkResult(())
+//  }
+//
+//  private def isNotReallyOk(writeResult: WriteResult) = !writeResult.ok || writeResult.writeErrors.nonEmpty || writeResult.writeConcernError.isDefined
+//
+//}
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,8 +8,6 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.4.0",
     "uk.gov.hmrc"             %% "play-frontend-govuk"              % "0.74.0-play-28",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"               % "0.69.0-play-28",
-    "uk.gov.hmrc"             %% "simple-reactivemongo"             % "8.0.0-play-28",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"               % "0.74.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"          % "0.74.0",
     "uk.gov.hmrc"             %% "pay-api-cor-card-payment-journey" % payApiVersion,
