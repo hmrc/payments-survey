83dbc915903279584c8a66ea22d7ce54e9a3dc1e
David Thornton <28890110+davidmthornton@users.noreply.github.com>
2022-11-18 15:32:00 +0000

    OPS-8371: Refactor out Repo abstract class

--- app/paysurvey/journey/JourneyRepo.scala
+++ app/paysurvey/journey/JourneyRepo.scala
@@ -1,20 +1,24 @@
 package paysurvey.journey
 
-import repository.Repo
-
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 import org.mongodb.scala.model.{Filters, IndexModel, IndexOptions}
 import org.mongodb.scala.model.Indexes.{ascending, descending}
 import uk.gov.hmrc.mongo.MongoComponent
+import uk.gov.hmrc.mongo.play.json.PlayMongoRepository
 
 @Singleton
 final class JourneyRepo @Inject() (mongo: MongoComponent)(implicit ec: ExecutionContext)
-  extends Repo("journey", mongo, Seq(IndexModel(ascending("journeyId"), IndexOptions()))) {
+  extends PlayMongoRepository[SurveyJourney](
+    mongoComponent = mongo,
+    collectionName = "journey",
+    indexes        = Seq(IndexModel(ascending("journeyId"))),
+    domainFormat   = SurveyJourney.format
+  ) {
+
   /**
    * Find the latest journey for given sessionId.
    */
-
   def findLatestJourneyByJourneyId(journeyId: SurveyJourneyId): Future[Option[SurveyJourney]] = {
     collection
       .withReadPreference(com.mongodb.ReadPreference.primary())
