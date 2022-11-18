48ec2120193ec59d691e8b7e05e35faef21207f2
Andy Dwelly <andydwelly@gmail.com>
2022-11-18 13:55:57 +0000

    Refactoring

--- app/paysurvey/journey/JourneyService.scala
+++ app/paysurvey/journey/JourneyService.scala
@@ -8,7 +8,7 @@ class JourneyService @Inject() (
     journeyRepo: JourneyRepo
 )(implicit ec: ExecutionContext) {
 
-  def insert(j: SurveyJourney): Future[Unit] = journeyRepo.insert(j) //.checkResult
+  def insert(j: SurveyJourney): Future[Unit] = journeyRepo.insert(j)
 
   def findLatestByJourneyId(journeyId: SurveyJourneyId): Future[Option[SurveyJourney]] = {
     journeyRepo.findLatestJourneyByJourneyId(journeyId)
