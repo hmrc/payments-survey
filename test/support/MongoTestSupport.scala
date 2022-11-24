bec6b24459e49b3ad6387bdf03b296ff805e90f2
Andy Dwelly <andydwelly@gmail.com>
2022-11-24 10:40:11 +0000

    Removed comments

--- test/support/MongoTestSupport.scala
+++ test/support/MongoTestSupport.scala
@@ -8,12 +8,6 @@ import uk.gov.hmrc.mongo.test.MongoSupport
 trait MongoTestSupport extends MongoSupport with BeforeAndAfterAll with BeforeAndAfterEach with Logging {
   self: Suite with ScalaFutures with AppendedClues =>
 
-  //longer timeout for dropping database or cleaning collections
-  //  private val longPatienceConfig = PatienceConfig(
-  //    timeout  = scaled(Span(6, Seconds)),
-  //    interval = scaled(Span(50, Millis)) //tests aren't run in parallel so why bother with waiting longer
-  //  )
-
   override def beforeEach(): Unit = {
     super.beforeEach()
     dropDatabase()
