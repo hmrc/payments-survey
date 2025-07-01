db7ded079cad412191d4aed83357cb637d1a1850
Jack Gilman <56401347+DreamcastKnight@users.noreply.github.com>
2025-07-01 13:43:44 +0100

    [JG] OPS-13537 - changing signout url (#57)
    * [JG] OPS-13537 - changing signout url
    
    * [JG] OPS-13537 - dependencies

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -35,7 +35,9 @@ class AppConfig @Inject() (config: ServicesConfig) {
   lazy val payFrontendBaseUrl: String = s"${config.getString("payFrontendBaseUrl")}/pay"
 
   val ggBaseUrl: String = loadConfig("ggBaseUrl") + "/gg"
-  val signOutUrl: String = s"$ggBaseUrl/sign-out"
+
+  val basGatewayBaseUrl: String = config.baseUrl("bas-gateway")
+  val signOutUrl: String = s"$basGatewayBaseUrl/bas-gateway/sign-out-without-state"
 
   lazy val analyticsToken: String = loadConfig(s"google-analytics.token")
   lazy val analyticsHost: String = loadConfig(s"google-analytics.host")
